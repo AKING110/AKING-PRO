ce6ea604809a7e177b6cb8a52cc020bb6e714171
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-10-19 18:04:32 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -5,5 +5,5 @@ if 'aarch' in arch:
     arch = 'aarch'
     print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools')
     import pro
-else:exit('\033[1;31m Sorry System or device not supported ")
+else:exit('\033[1;31m Sorry System or device not supported ')
     
