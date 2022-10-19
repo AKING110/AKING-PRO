6bd244b096e93ca2ca00295e08f0f189cd855a3a
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-10-19 18:03:33 +0500

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,9 @@
+import os
+from platform import uname
+arch=uname().machine.lower()
+if 'aarch' in arch:
+    arch = 'aarch'
+    print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools')
+    import pro
+else:exit('\033[1;31m Sorry System or device not supported ")
+    
