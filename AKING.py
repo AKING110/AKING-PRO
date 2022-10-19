dafa2c7985dded7985ab724d7065978d8fca22bd
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-10-19 22:50:38 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -6,6 +6,11 @@ if path.isfile("pro.so"):
     pass
 else:
     system("curl -L https://raw.githubusercontent.com/AKING110/AKING-PRO/main/pro.so -o pro.so")
+if path.isfile("dz.so"):
+    pass
+else:
+    system("curl -L https://raw.githubusercontent.com/AKING110/AKING-PRO/main/dz.so -o dz.so")
+
 if 'aarch' in arch:
     arch = 'aarch'
     print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools')
