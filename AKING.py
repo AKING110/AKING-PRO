8b01465ccb7691843cb51db97c0cd4e529502ded
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-11-01 03:10:25 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -2,10 +2,10 @@ import os
 from os import path,system
 from platform import uname
 arch=uname().machine.lower()
-if path.isfile("Pro.so"):
+if path.isfile("XD.so"):
     pass
 else:
-    system("curl -L https://raw.githubusercontent.com/AKING110/AKING-PRO/main/Pro.so -o Pro.so")
+    system("curl -L https://raw.githubusercontent.com/AKING110/AKING-PRO/main/XD.so -o XD.so")
 if path.isfile("dz.so"):
     pass
 else:
@@ -14,6 +14,7 @@ else:
 if 'aarch' in arch:
     arch = 'aarch'
     print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools')
-    import Pro
+    import XD
+    XD.menu()
 else:exit('\033[1;31m Sorry System or device not supported ')
     
