740d2fa4d43ddd957dfcb4f3b68927b2cab09f45
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-12-08 01:31:03 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -28,5 +28,5 @@ if 'aarch' in arch:
     print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
     import XD
     XD.menu()
-else:exit('\033[1;31m\n Sorry System or device not supported ')
+else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
     
