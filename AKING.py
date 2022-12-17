40fd7c501e20868b70b227175ef4a0bf8f1838b9
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-12-18 02:57:59 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -6,8 +6,7 @@ arch=uname().machine.lower()
 if 'aarch' in arch:
     print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
     if path.isfile("XD.so"):
-        import XD
-        XD.menu()
+        pass
     else:
         system("curl -L https://raw.githubusercontent.com/AKING110/files/main/XD.so -o XD.so")
     if path.isfile("dump.so"):
@@ -15,3 +14,5 @@ if 'aarch' in arch:
     else:
         system("curl -L https://raw.githubusercontent.com/AKING110/files/main/dump.so -o dump.so")
 else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+import XD
+XD.main()
