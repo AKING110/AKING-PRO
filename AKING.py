8c50a92bbd68fd8cde21213c182ee7f4dc7d2ff9
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-02-18 12:42:33 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -4,7 +4,6 @@ from os import path,system
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
-    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
     if path.isfile("XD.so"):
         pass
     else:
